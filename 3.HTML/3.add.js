d608108a80969c7337df24a89bd8223b1997a508
lowkieman <lowkieman@gmail.com>
2024-10-13 01:14:49 +0300

    adding html elements

---
+++ 3.HTML/3.add.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    /*
+    * append()
+    * prepend()
+    * after()
+    * before()
+    */
+   $(".p12").append("This text is appended using .append()");
+   $(".p12").prepend("This paragraph is done using .prepend()");
+   $("img").after(" after() method");
+   $("img").before("before()")
+
+})
